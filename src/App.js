0cedd221006c66440d2dde8ebed1cf4a85748854
Saitej-47 <saitej.pasupuleti47@gmail.com>
2025-11-06 10:37:17 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Evaluation System</h1>
+        <p>Welcome to the Evaluation System Application</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
